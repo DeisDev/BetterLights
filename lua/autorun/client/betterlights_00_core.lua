c5aaf23a471efa6a8e77978b7a4f2b287390234d
DeisDev <163078690+DeisDev@users.noreply.github.com>
2026-07-04 14:31:29 -0600

    v1.6.0-beta4

--- lua/autorun/client/betterlights_00_core.lua
+++ lua/autorun/client/betterlights_00_core.lua
@@ -1,7 +1,7 @@
 if CLIENT then
     local BL = BetterLights
 
-    BL.VERSION = "v1.6.0-beta3"
+    BL.VERSION = "v1.6.0-beta4"
 
     BL._networkHandlers = BL._networkHandlers or {}
     BL._clientConVars = BL._clientConVars or {}
